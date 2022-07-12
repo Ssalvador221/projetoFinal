825f2779cdb524023fc392afe18c6d740d82b136
Ssalvador221 <j8731615@gmail.com>
2022-07-11 23:40:18 -0300

    git commit 2

--- app/src/main/java/com/example/projetofinal/thirdFragment.java
+++ app/src/main/java/com/example/projetofinal/thirdFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class thirdFragment extends Fragment {
+
+
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,6 +59,8 @@ public class thirdFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
 
